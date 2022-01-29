e9f22f38256986d89406c5cb759a8ca842464f7d
Loopy <91286502+Wizzable@users.noreply.github.com>
2022-01-29 21:09:47 +0000

    Swiched to inputs and got rid of useless stuff

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import discum 
-token = "TOKENHERE" #  Your account token
-guild_id = 'GUILD_ID_HERE' # The guild you wanna scrape id
-channel_id = 'CHANNEL_ID_HERE' # get the id of a channel that most people can see like #rules etc
+token = input("Please provide a token: ") 
+guild = input("Please provide a guild id: ") 
+channel = input("Please provide a channel id: ") 
 bot = discum.Client(token= token, log=True)
 
-bot.gateway.fetchMembers(guild_id, channel_id, keep=['username','discriminator'],startIndex=0, method='overlap',wait=1)
+bot.gateway.fetchMembers(guild, channel, startIndex=0, method='overlap',wait=1)
 @bot.gateway.command
 def memberTest(resp):
-    if bot.gateway.finishedMemberFetching(guild_id):
-        lenmembersfetched = len(bot.gateway.session.guild(guild_id).members)
-        print(str(lenmembersfetched)+' members fetched')
+    if bot.gateway.finishedMemberFetching(guild):
         bot.gateway.removeCommand(memberTest)
         bot.gateway.close()
 
 bot.gateway.run()
 
 with open('result.txt', 'w', encoding="utf-8") as file :
-    for memberID in bot.gateway.session.guild(guild_id).members:
+    for memberID in bot.gateway.session.guild(guild).members:
         id = str(memberID)
-        user = str(bot.gateway.session.guild(guild_id).members[memberID].get('username'))
+        user = str(bot.gateway.session.guild(guild).members[memberID].get('username'))
         print(f'ID: {id}\n')
         file.write(f'{id}\n')
            
